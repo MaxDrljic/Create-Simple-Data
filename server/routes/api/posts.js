795c5a1080e9c16837b80f826adeecbabd5bfc48
MaxDrljic <max_spree@hotmail.com>
2018-10-15 21:08:50 +0200

    Routing, connecting to MongoDB & defining the GET request for posts

---
+++ server/routes/api/posts.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Post
+// ('/') is referenced to /api/posts declared in index.js
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+// Add Post
+
+
+// Delete Post
+
+
+// Connects to the mLab db by passing the URL provided by the database
+// useNewUrlParser: true - Fixes the error / warning thrown by mongo
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect('mongodb://max123:max123@ds013320.mlab.com:13320/vue_express', {
+    useNewUrlParser: true
+  })
+
+  return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
